a2b2deea5f6fdd95198727579ac90fb7826d0ed9
HungYn <72488227+HungYn@users.noreply.github.com>
2023-02-26 10:30:36 +0800

    Add files via upload

---
+++ WiFi_scan_Networks_reorder.ino
@@ -0,0 +1,58 @@
+
+#include "WiFi.h"
+
+void setup()
+{
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  Serial.println("Setup done");
+}
+
+void loop()
+{
+  Serial.println("scan start");
+
+  int n = WiFi.scanNetworks();
+  Serial.println("掃描完成");
+  if (n == 0) {
+    Serial.println("沒有發現任何網路");
+  } else {
+    Serial.print(n);
+    Serial.println(" 個網路被發現");
+
+    // 將網路依 RSSI 排序
+    int indices[n];
+    for (int i = 0; i < n; ++i) {
+      indices[i] = i;
+    }
+    for (int i = 0; i < n - 1; ++i) {
+      for (int j = i + 1; j < n; ++j) {
+        if (WiFi.RSSI(indices[j]) > WiFi.RSSI(indices[i])) {
+          // 交換位置
+          int tmp = indices[i];
+          indices[i] = indices[j];
+          indices[j] = tmp;
+        }
+      }
+    }
+
+    // 依序列印排序後的網路資訊
+    for (int i = 0; i < n; ++i) {
+      int j = indices[i];
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(j));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(j));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(j) == WIFI_AUTH_OPEN) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  // Wait a bit before scanning again
+  delay(5000);
+}
